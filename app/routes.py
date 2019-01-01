13c042a11e2182bb43eb998aa1f250bde2d60483
Jeandre Rohlandt <jrohlandt@gmail.com>
2019-01-01 17:37:49 +0200

    Views and templates.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'jeandre'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Welcome to 2019, the future is here!'
+        },
+        {
+            'author': {'username': 'Jane'},
+            'body': 'Indeed, no flying cars but driveless cars are better anyway.'
+        },
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
